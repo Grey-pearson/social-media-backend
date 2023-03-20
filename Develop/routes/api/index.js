7d0a48af5ec3280f81592409cc60a9a8726afebe
Grey P <greytpearson@gmail.com>
2023-03-20 14:15:20 -0600

    added git ignore

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,10 +1,7 @@
-const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
-const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./studentRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
