788761e622a689fc35666296e9d79fc51459f2f5
Grey P <greytpearson@gmail.com>
2023-04-13 11:36:50 -0600

    updated readme and small error in userController

--- Develop/controllers/userController.js
+++ Develop/controllers/userController.js
@@ -5,7 +5,13 @@ module.exports = {
     getAllUsers(req, res) {
         User.find({})
             .select('-__v')
-            .then(dbUserData => res.json(dbUserData))
+            .then(dbUserData => { // error
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
--- README.md
+++ README.md
@@ -6,4 +6,4 @@ this is a backend app indended to be used with a social media app that has user 
 
 ability to create new user profiles that make sure users credentials are unique, ability to create thoughts(posts) that connect to the user model, and reactions(comments) that run throught the reaction schema to create an array of comments.
 
-[video tutorial demonstrates how it works](video)
+[video tutorial demonstrates how it works](https://watch.screencastify.com/v/7HP0QrZAkDxD8nmTIa8X)
